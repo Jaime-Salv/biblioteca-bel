bfd9124bffc31bbf1e6bf0197474f8aba2132cec
Jaime-Salv <jaime.rugomez@gmail.com>
2026-09-03 12:40:31 +0200

    Load collection-specific styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles/global.css'
+import './styles/collection.css'
 import { AuthProvider } from './context/AuthContext'
 import { LibraryProvider } from './context/LibraryContext'
 
